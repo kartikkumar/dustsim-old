572ef1dccee54e2382112d762cd693cffeb690bf
Kartik Kumar <me@kartikkumar.com>
2014-07-24 22:58:00 +0200

    Update layout of all file headers. Rename Applications/ sub-directory to ApplicationModes/ and change filename of single simulator.

--- Dustsim/InputOutput/caseData.cpp
+++ Dustsim/InputOutput/caseData.cpp
@@ -1,9 +1,9 @@
 /*    
- *    Copyright (c) 2010-2014, Delft University of Technology
- *    Copyright (c) 2010-2014, K. Kumar (me@kartikkumar.com)
- *    Copyright (c) 2013-2014, S. Hirsh (sethhirsh@berkeley.edu)
- *    All rights reserved.
- *    See http://bit.ly/12SHPLR for license details.
+ * Copyright (c) 2010-2014, Delft University of Technology
+ * Copyright (c) 2010-2014, K. Kumar (me@kartikkumar.com)
+ * Copyright (c) 2013-2014, S. Hirsh (sethhirsh@berkeley.edu)
+ * All rights reserved.
+ * See http://bit.ly/12SHPLR for license details.
  */
 
 // #define NEGATIVE_ZERO -std::numeric_limits< double >::min( )
